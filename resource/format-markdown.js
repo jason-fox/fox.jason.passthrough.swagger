0e85a67b652426b84dd51f5bf1bb9a4a3964152e
Jason Fox <jason.fox@fiware.org>
2019-08-28 08:33:55 +0200

    Reformating Output to use section and example

--- resource/format-markdown.js
+++ resource/format-markdown.js
@@ -17,24 +17,40 @@ function moveAnchors(markdown) {
   var text = [];
   var id = null;
   var codeblock = true;
+  var swaggerVerb = "";
+  var firstTopic = true;
 
   // Read each line of the Markdown file in turn
   for (var i = 0; i < lines.length; i++) {
-    if (lines[i].startsWith("<a name")) {
+    if (firstTopic && lines[i].startsWith("# ")){
+      text.push(lines[i] + " {.swagger}");
+      firstTopic == false;
+    } else if (lines[i].startsWith("<a name")) {
       var start = lines[i].indexOf('"') + 1;
       var end = lines[i].indexOf('"', start);
       id = lines[i].substring(start, end);
       text.push("");
-    } else if (id === null && lines[i].startsWith("#####")) {
+    } else if (id === null && lines[i].startsWith("### ")) {
       text.push(lines[i] + " {.section}");
+    } else if (id === null && lines[i].startsWith("#### ")) {
+      text.push("#" + lines[i] + " {.section}");
+    } else if (id === null && lines[i].startsWith("#####")) {
+      text.push(lines[i] + " {.example}");
     } else if (id !== null && lines[i].startsWith("#")) {
-      text.push(lines[i] + " {#" + id + "}");
+      if(lines[i+1].equals("```")){
+        var arr = lines[i+2].split(' ');
+        var verb =  arr[0].toLowerCase();
+        text.push(lines[i] + " {#" + id + " .swagger-" + verb +"}");
+      } else { 
+        text.push(lines[i] + " {#" + id + "}");
+      }
       id = null;
     } else if (lines[i].startsWith("```")){
       if(codeblock){
         if(lines[i].equals("```")){
           var arr = lines[i+1].split(' ');
-          text.push("```swagger-" + arr[0].toLowerCase());
+          swaggerVerb = "swagger-" + arr[0].toLowerCase();
+          text.push("```" + swaggerVerb);
         } else {
           text.push(lines[i]);
         }
