0276d990384538e58c0edc1c4419326cc3316fe3
GitHub Action <action@github.com>
2022-11-14 12:32:57 +0100

    Install Nodejs as part of 4.0 tests since Nashorn has been removed.

--- test/setup.sh
+++ test/setup.sh
@@ -1,5 +1,14 @@
+#!/bin/sh
+# This file is part of the  DITA-OT Swagger Plug-in project.
+set -e
+
 curl -sLO https://github.com/jgm/pandoc/releases/download/2.10.1/pandoc-2.10.1-1-amd64.deb
 dpkg -i pandoc-2.10.1-1-amd64.deb
 pandoc --version
 echo
 which pandoc
+
+apt-get update -q
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -qy --no-install-recommends nodejs
+nodejs -v
