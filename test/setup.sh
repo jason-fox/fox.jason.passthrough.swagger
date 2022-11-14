9ff74e6d1e58f23bae6471312adb6d53a2182e92
GitHub Action <action@github.com>
2022-11-14 18:33:54 +0100

    Update to Pandoc 2.18

--- test/setup.sh
+++ test/setup.sh
@@ -2,8 +2,8 @@
 # This file is part of the  DITA-OT Swagger Plug-in project.
 set -e
 
-curl -sLO https://github.com/jgm/pandoc/releases/download/2.10.1/pandoc-2.10.1-1-amd64.deb
-dpkg -i pandoc-2.10.1-1-amd64.deb
+curl -sLO https://github.com/jgm/pandoc/releases/download/2.18/pandoc-2.18-1-amd64.deb
+dpkg -i pandoc-2.18-1-amd64.deb
 pandoc --version
 echo
 which pandoc
